8de8ca8040cb088044cb8de26d07833060ec9a1f
Masutier <masutier@gmail.com>
2023-09-22 15:26:21 -0500

    change in texts formats and dates update

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+import json
+from flask import Flask, render_template as render, flash, redirect, url_for, send_file
+
+with open("/etc/cine.json") as config_file:
+    config = json.load(config_file)
+
+app = Flask(__name__, static_folder='static')
+app.config['SECRET_KEY'] = config['SECRET_KEY']
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+
+    return render('index.html')
+
+
+@app.route('/category')
+def category():
+
+    return render('categoria.html')
+
+
+@app.route('/previous')
+def previous():
+
+    return render('previous.html')
+
+
+@app.route('/ganadores')
+def ganadores():
+
+    return render('ganadores.html')
+
+
+if __name__ == '__main__':
+    #app.run(debug=True, host="172.16.170.129", port=8080)
+    app.run(debug=True, host="localhost", port=8080)
+
